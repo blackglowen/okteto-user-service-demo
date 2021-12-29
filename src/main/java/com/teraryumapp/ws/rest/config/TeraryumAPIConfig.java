b4dbd5b9239b57fc4200a7952f90a3fe327e3282
Touré A. Karim <trabdlkarim@programmer.net>
2021-12-30 00:20:24 +0300

    update to remove useless lines

--- src/main/java/com/teraryumapp/ws/rest/config/TeraryumAPIConfig.java
+++ src/main/java/com/teraryumapp/ws/rest/config/TeraryumAPIConfig.java
@@ -6,16 +6,10 @@ import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
 import org.eclipse.microprofile.openapi.annotations.info.Contact;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.eclipse.microprofile.openapi.annotations.info.License;
-import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+// import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 @ApplicationPath("/api/rest")
-@OpenAPIDefinition(tags = {
-                @Tag(name = "user"),
-                @Tag(name = "resource"),
-                @Tag(name = "entity"),
-                @Tag(name = "api"),
-                @Tag(name = "REST"),
-}, info = @Info(title = "Teraryum Web Services API", version = "v1.0.0.", contact = @Contact(name = "TeraryumApp", url = "http://teraryumapp.com/contact", email = "teraryumapp@gmail.com"), license = @License(name = "Licence: MIT", url = "https://opensource.org/licenses/MIT")))
+@OpenAPIDefinition(info = @Info(title = "Teraryum WS REST API", version = "v1.0.0", contact = @Contact(name = "TeraryumApp", url = "http://teraryumapp.com/contact", email = "teraryumapp@gmail.com"), license = @License(name = "Licence: MIT", url = "https://opensource.org/licenses/MIT")))
 public class TeraryumAPIConfig extends Application {
 
 }
