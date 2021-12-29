9a559ac7c453a8e5f8b0aacc978aebb6236631e4
Touré A. Karim <trabdlkarim@programmer.net>
2021-12-29 14:10:23 +0300

    add project resource related files

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO users( fname, lname, username, email, password, phone, birthdate, created_at, updated_at, status ) 
+VALUES('Habdool', 'Khareem', 'h.khareem', 'trabdlkarim@gmail.com', '$2a$10$prLw9rMNpBqNxdfIIfUPT.nRbvSEtP7Wb6lokYfagyytxhC.gtdwq', '+905348506190', '1993-06-03', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ACTIVE'), 
+      ('John', 'Doe', 'johndoe', 'john.doe@gmail.com', '$2a$10$prLw9rMNpBqNxdfIIfUPT.nRbvSEtP7Wb6lokYfagyytxhC.gtdwq', '+14124501600', '1986-10-17', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ACTIVE');
